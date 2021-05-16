b3c0bda091822b162311c20302edd0c93231428e
Bruno Dahlem <bruno.dahlem2@gmail.com>
2021-05-16 12:18:37 +0200

    Begin dice code

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ let name1 = document.getElementById('name1');
 let name2 = document.getElementById('name2');
 let avatarPlayer1 = document.getElementById('avatarPlayer1');
 let avatarPlayer2 = document.getElementById('avatarPlayer2');
+let roll = document.getElementById('roll');
+let current1 = document.getElementById('currentScore1');
+let current2 = document.getElementById('currentScore2');
+let global1 = document.getElementById('globalScore1');
+let global2 = document.getElementById('globalScore2');
+
 
 // Personnalisation du nom des joueurs
 btnModal.addEventListener('click', function() {
@@ -12,6 +18,10 @@ btnModal.addEventListener('click', function() {
   name2.textContent = secondPlayerName.value;
   
   gambling.play()
+  current1.value = 0;
+  current2.value = 0;
+  global1.value = 0;
+  global2.value = 0;
 });
 
 // personnalisation de l'avatar
@@ -43,7 +53,31 @@ avatarPlayer2.addEventListener('click', function() {
   } else if (mySrc === './pictures/crystal-smiley.png') {
     avatarPlayer2.setAttribute('src', './pictures/face-angel.png');
   }
-
-  
 });
 
+
+function effectuerLanceDe() {
+  roll.addEventListener('click', lanceDe);
+};
+
+function lanceDe() {
+  let de = Math.ceil(Math.random() * 6);
+  current1.value = de;
+  // Affiche l'image du dé en fonction de la valeur de random
+  let imgDice = document.getElementById('img-dice');
+  if (de === 1) {
+    imgDice.setAttribute('src', './pictures/dice-1.png');
+  } else if (de === 2) {
+    imgDice.setAttribute('src', './pictures/dice-2.png');
+  } else if (de === 3) {
+    imgDice.setAttribute('src', './pictures/dice-3.png');
+  } else if (de === 4) {
+    imgDice.setAttribute('src', './pictures/dice-4.png');
+  } else if (de === 5) {
+    imgDice.setAttribute('src', './pictures/dice-5.png');
+  } else if (de === 6) {
+    imgDice.setAttribute('src', './pictures/dice-6.png');
+  } else {
+    imgDice.setAttribute('src', './pictures/dice-1.png');
+  }
+};
